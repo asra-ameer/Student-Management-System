be8fc03923ffdcef367865f2d9deebdc98264ae2
asra-ameer <151917440+asra-ameer@users.noreply.github.com>
2025-07-28 12:09:41 +0530

    set up the database uisng sql server management system

---
+++ Student Management System Project/Student Management System Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Management_System_Project.Model.Entity;
+
+namespace Student_Management_System_Project.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options) {
+
+        }
+        public  DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Student Management System Project/Student Management System Project/Model/Entity/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Student_Management_System_Project.Model.Entity
+{
+    public class Student
+    {
+        public Guid id { get; set; }
+        public  required string  Name { get; set; }
+        public required string Email { get; set; }
+        public required string FollowingDegree { get; set; }
+        public  string? Address { get; set; }
+    }
+}
