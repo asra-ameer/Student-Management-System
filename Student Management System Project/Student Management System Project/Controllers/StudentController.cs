d16f231a7c19fe54fce288816ef4e86f6cd90f52
asra-ameer <151917440+asra-ameer@users.noreply.github.com>
2025-07-28 15:11:50 +0530

    created crud operations for student management System

--- Student Management System Project/Student Management System Project/Controllers/StudentController.cs
+++ Student Management System Project/Student Management System Project/Controllers/StudentController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Student_Management_System_Project.Data;
+using Student_Management_System_Project.Model;
+using Student_Management_System_Project.Model.Entity;
 
 namespace Student_Management_System_Project.Controllers
 {
@@ -7,5 +11,71 @@ namespace Student_Management_System_Project.Controllers
     [ApiController]
     public class StudentController : ControllerBase
     {
+        private readonly ApplicationDbContext dbContext;
+        public StudentController(ApplicationDbContext dbContext) {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult getStudents() {
+            return Ok(dbContext.Students.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult addStudents(AddStudentsDto addStudentsDto)
+        {
+            var studentsEntity = new Student()
+            {
+                
+                Name = addStudentsDto.Name,
+                Email = addStudentsDto.Email,
+                FollowingDegree = addStudentsDto.FollowingDegree,
+                Address = addStudentsDto.Address,
+            };
+            dbContext.Students.Add(studentsEntity);
+            dbContext.SaveChanges();
+            return Ok(dbContext.Students);
+        }
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult getStudentsById(Guid id)
+        {
+            var student = dbContext.Students.Find(id);
+            if (student is null) {
+             return NotFound($"Student with id{id} is not available");
+            }
+            return Ok(student);
+        }
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult updateStudentsById(Guid id,UpdateStudentsDto updateStudetsDto)
+        {
+            var student = dbContext.Students.Find(id);
+            if (student is null)
+            {
+                return NotFound($"Student with id{id} is not available");
+            }
+            
+            student.Name=updateStudetsDto.Name;
+            student.Email=updateStudetsDto.Email;
+            student.FollowingDegree=updateStudetsDto.FollowingDegree;
+            student.Address=updateStudetsDto.Address;
+
+            dbContext.SaveChanges();
+            return Ok(student);
+        }
+        [HttpDelete]
+        public IActionResult deleteStudentsById(Guid id) { 
+            var student=dbContext.Students.Find(id);
+            if (student is null) {
+                return NotFound($"Student with id:- {id} not found");
+            }
+            dbContext.Students.Remove(student);
+            dbContext.SaveChanges();
+            return Ok($"Sucessfully removed student with id:- {id}");
+        }
+
+
+
     }
 }
---
+++ Student Management System Project/Student Management System Project/Model/UpdateStudentsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Student_Management_System_Project.Model
+{
+    public class UpdateStudentsDto
+    {
+        public Guid id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string FollowingDegree { get; set; }
+        public string? Address { get; set; }
+    }
+}
